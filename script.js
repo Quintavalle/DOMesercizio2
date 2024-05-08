b8b44508e01eb3d7f3ec271514439c313f0de79a
Giorgio <147600523+Quintavalle@users.noreply.github.com>
2024-05-08 23:05:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const paragrafi = document.querySelectorAll('.paragrafo');
+console.log("Elementi con classe 'paragrafo':", paragrafi);
+
+const elemento3 = document.getElementById('elemento-3');
+console.log("Elemento con id 'elemento-3':", elemento3);
+
+const h2Elements = document.querySelectorAll('h2');
+console.log("Elementi h2 della pagina:", h2Elements);
